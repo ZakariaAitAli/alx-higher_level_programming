b2d24069f8bd5bc2ee640c2e44626b8bfc012f2d
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-01-11 23:12:44 +0100

    Task 3: handle error

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street")
+print(f"{number} Battery street") if isinstance(number, int) else \
+    print(f"ValueError: Unknown format code 'd' for object of type '{type(number).__name__}'")
+
