599e9554747cb3067aa2ad0eff85247541648267
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-01-11 23:20:29 +0100

    Task 3: Fix Pycodestyle validation

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 number = 98
 print(f"{number} Battery street") if isinstance(number, int) \
-    else print(f"ValueError: Unknown format code 'd' for object of type '{type(number).__name__}'")
+    else print(f"ValueError: Unknown format code 'd' for"\
+    " object of type '{type(number).__name__}'")
