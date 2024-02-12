47166c4d2bbae9ba9f824ad1246e470c7953c6e6
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-12 22:13:16 +0100

    Task 9: Update #1

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -126,5 +126,21 @@ class TestRectangle(unittest.TestCase):
 		r.update(89, 2, 3, 4, 5)
 		self.assertEqual(str(r), "[Rectangle] (89) 4/5 - 2/3")
 
+	def test_update_with_keyword_arguments(self):
+		r = Rectangle(10, 10, 10, 10)
+		self.assertEqual(str(r), "[Rectangle] (1) 10/10 - 10/10")
+
+		r.update(height=1)
+		self.assertEqual(str(r), "[Rectangle] (1) 10/10 - 10/1")
+
+		r.update(width=1, x=2)
+		self.assertEqual(str(r), "[Rectangle] (1) 2/10 - 1/1")
+
+		r.update(y=1, width=2, x=3, id=89)
+		self.assertEqual(str(r), "[Rectangle] (89) 3/1 - 2/1")
+
+		r.update(x=1, height=2, y=3, width=4)
+		self.assertEqual(str(r), "[Rectangle] (89) 1/3 - 4/2")
+
 if __name__ == '__main__':
 	unittest.main()
