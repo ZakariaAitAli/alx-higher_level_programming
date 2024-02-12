a4bf6f9ab9bbe14633f67263ec5436395ae480ab
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-12 21:09:35 +0100

    Task 3: Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(Base):
 	@width.setter
 	def width(self, value):
 		"""width setter"""
+		if type(value) != int:
+			raise TypeError("width must be an integer")
+		if value <= 0:
+			raise ValueError("width must be > 0")
 		self.__width = value
 
 	@property
@@ -41,6 +45,10 @@ class Rectangle(Base):
 	@height.setter
 	def height(self, value):
 		"""height setter"""
+		if type(value) != int:
+			raise TypeError("height must be an integer")
+		if value <= 0:
+			raise ValueError("height must be > 0")
 		self.__height = value
 
 	@property
@@ -51,6 +59,10 @@ class Rectangle(Base):
 	@x.setter
 	def x(self, value):
 		"""x setter"""
+		if type(value) != int:
+			raise TypeError("x must be an integer")
+		if value < 0:
+			raise ValueError("x must be >= 0")
 		self.__x = value
 
 	@property
@@ -61,6 +73,10 @@ class Rectangle(Base):
 	@y.setter
 	def y(self, value):
 		"""y setter"""
+		if type(value) != int:
+			raise TypeError("y must be an integer")
+		if value < 0:
+			raise ValueError("y must be >= 0")
 		self.__y = value
 
 	def area(self):
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,14 +6,36 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
 
-    def test_id_generation(self):
+	def test_id_generation(self):
 
-        r1 = Rectangle(10, 2, id=1)
-        r2 = Rectangle(2, 10, id=2)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r3.id, 12)
+		r1 = Rectangle(10, 2, id=1)
+		r2 = Rectangle(2, 10, id=2)
+		r3 = Rectangle(10, 2, 0, 0, 12)
+		self.assertEqual(r1.id, 1)
+		self.assertEqual(r2.id, 2)
+		self.assertEqual(r3.id, 12)
+
+	def test_height_string(self):
+		with self.assertRaises(TypeError) as cm:
+			Rectangle(10, "2")
+		self.assertEqual(str(cm.exception), "height must be an integer")
+
+	def test_width_negative_value(self):
+		with self.assertRaises(ValueError) as cm:
+			r = Rectangle(10, 2)
+			r.width = -10
+		self.assertEqual(str(cm.exception), "width must be > 0")
+
+	def test_x_invalid_value(self):
+		with self.assertRaises(TypeError) as cm:
+			r = Rectangle(10, 2)
+			r.x = {}
+		self.assertEqual(str(cm.exception), "x must be an integer")
+
+	def test_y_negative_value(self):
+		with self.assertRaises(ValueError) as cm:
+			Rectangle(10, 2, 3, -1)
+		self.assertEqual(str(cm.exception), "y must be >= 0")
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
