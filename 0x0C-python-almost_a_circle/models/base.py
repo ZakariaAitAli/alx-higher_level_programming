6f9010bf77ca5d97d41113e04a566a5a2cb36a06
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-13 15:28:18 +0100

    Convert indentation to spaces

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,18 +5,18 @@
 """Base class"""
 
 class Base:
-	"""Base class"""
-	__nb_objects = 0
-	
-	def __init__(self, id=None):
-		"""Base class constructor"""
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    """Base class"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Base class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-	def reset_id():
-		"""reset the id"""
-		Base.__nb_objects = 0
+    def reset_id():
+        """reset the id"""
+        Base.__nb_objects = 0
 
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,21 +5,21 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
-	def test_id_generation(self):
-		b1 = Base()
-		b2 = Base()
-		b3 = Base()
-		self.assertEqual(b2.id, b1.id + 1)
-		self.assertEqual(b3.id, b2.id + 1)
+    def test_id_generation(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b2.id, b1.id + 1)
+        self.assertEqual(b3.id, b2.id + 1)
 
-	def test_custom_id(self):
-		b4 = Base(12)
-		self.assertEqual(b4.id, 12)
+    def test_custom_id(self):
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
 
-	def test_incremental_id(self):
-		b5 = Base()
-		b6 = Base()
-		self.assertEqual(b6.id, b5.id + 1)
+    def test_incremental_id(self):
+        b5 = Base()
+        b6 = Base()
+        self.assertEqual(b6.id, b5.id + 1)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
