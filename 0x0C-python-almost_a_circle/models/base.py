33e7627cf6bbda2854469b49b9ccfc5701f113c6
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-13 17:25:46 +0100

    tried to fix Pycodestyle validation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,32 +2,20 @@
 # 0x0C. Python - Almost a circle
 # Zakaria AIT ALI <zakariaaitali555@gmail.com>
 
-"""Defines a base model class."""
+"""Base class"""
 
 class Base:
-    """Represent the base model.
-
-    Represents the "base" for all other classes in project 0x0C*.
-
-    Attributes:
-        __nb_objects (int): The number of instantiated Bases.
-    """
-
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
-        """
+        """Base class constructor"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    def reset_id():
+    def reset_id(self):
         """reset the id"""
-        Base.__nb_objects = 0
-  
+        self.__nb_objects = 0
