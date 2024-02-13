49b5414e6f627353a23e5b63ab839465a7f3affb
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-13 16:48:10 +0100

    Task 1: base class with ists unites

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+# 0x0C. Python - Almost a circle
+# Zakaria AIT ALI <zakariaaitali555@gmail.com>
+
+"""Base class"""
+
+class Base:
+	"""Base class"""
+	__nb_objects = 0
+	
+	def __init__(self, id=None):
+		"""Base class constructor"""
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
+
+	def reset_id():
+		"""reset the id"""
+		Base.__nb_objects = 0
+  
